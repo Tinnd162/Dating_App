700272b9134c929f8f32b601618ed613e861d437
Tinnd162 <tinnd162@gmail.com>
2021-07-25 14:44:32 +0700

    update resources api, client.

---
+++ API/Data/Migrations/20210725054150_updateEntities.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class updateEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Counntry",
+                table: "Users",
+                newName: "Country");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "Users",
+                newName: "Counntry");
+        }
+    }
+}
